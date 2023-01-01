a91cee6a577b8851caf17a08cadfbb8839a6ac5c
kiung22 <rldnd22@gmail.com>
2023-01-01 14:58:25 +0900

    first project setting

---
+++ src/main/kotlin/com/sincheon/ssadagame/SsadagameApplication.kt
@@ -0,0 +1,11 @@
+package com.sincheon.ssadagame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsadagameApplication
+
+fun main(args: Array<String>) {
+    runApplication<SsadagameApplication>(*args)
+}
---
+++ src/test/kotlin/com/sincheon/ssadagame/SsadagameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sincheon.ssadagame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsadagameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
